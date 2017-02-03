f717d4648b13c9b958baa3bf6ab67bcfc57d14cb
jahapaula <salla.laaksonen@iki.fi>
2017-02-03 20:20:11 +0200

    Finalizing chapter2

--- students2014regression.R
+++ students2014regression.R
@@ -5,7 +5,7 @@ library(GGally)
 # reads the full data set fro the given url to R project
 students2014 <- read.csv("data/lrn14_final.csv", row.names = 1, header=TRUE)
 
-# The dataset includes 166 rows of data with 7 variables. 
+# The dataset includes 166 rows of data with 7 variables. Doing diagnostics with str and dim functions.
 str(students2014)
 dim(students2014)
 
@@ -16,19 +16,22 @@ pic
 #pairs(students2014[-1], col = learning2014$gender)
 #pairs(students2014[-1])
 
-# summary provides the descriptive statistics of the dataset
+# summary provides the descriptive statistics of the dataset (see table 1)
 summary(students2014)
 # n = 166, 110 female, 56 male, Age M = 25.51)
 
-#examine the structure to check if everything is ok
-str(lrn14_final)
+# create a regression model with three explanatory variables to explain the variation of the variable Points.
+model1 <- lm(Points ~ Attitude + stra + surf, data = students2014)
+summary(model1)
 
-# save the output to csv file 
-write.csv(lrn14_final, file = "data/lrn14_final.csv")
+# create a better model
+model2 <- lm(Points ~ Attitude + stra, data = students2014)
+summary(model2)
 
-# open the file
-nwedata <- read.csv("data/lrn14_final.csv", row.names = 1)
+# try once more with attitude + surf
+model3 <- lm(Points ~ Attitude + surf, data = students2014)
+summary(model3)
 
-# for diagnostics
-str(nwedata)
-head(nwedata, 10)
+# print diagnostic plots: Residuals vs Fitted values, Normal QQ-plot and Residuals vs Leverage.
+plot(model2, which = c(1, 2, 5))
+# btw this seems like a legit prettier option https://rpubs.com/therimalaya/43190
